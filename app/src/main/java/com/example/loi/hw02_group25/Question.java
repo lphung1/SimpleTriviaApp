480b35d63368c7a06cf82e4454cbab1cafc0a900
lphung1 <loi_phung81@yahoo.com>
2018-03-04 03:35:36 -0500

    Added next button functionality and timer

--- app/src/main/java/com/example/loi/hw02_group25/Question.java
+++ app/src/main/java/com/example/loi/hw02_group25/Question.java
@@ -1,6 +1,7 @@
 package com.example.loi.hw02_group25;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,21 +14,38 @@ import java.util.ArrayList;
 
 public class Question extends AppCompatActivity {
 
+    int position = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
         setTitle("Trivia Time");
-        ImageView triviaImage = findViewById(R.id.questionImage);
-        TextView textQuestion = findViewById(R.id.questionDescriptionText);
-        RadioButton rb1 = findViewById(R.id.question1);
-        RadioButton rb2 = findViewById(R.id.question2);
-        RadioButton rb3 = findViewById(R.id.question3);
-        RadioButton rb4 = findViewById(R.id.question4);
 
-        int position = 0;
+        final ImageView triviaImage = findViewById(R.id.questionImage);
+        final TextView textQuestion = findViewById(R.id.questionDescriptionText);
+        final TextView timeText = findViewById(R.id.timeLeftText);
+        final RadioButton rb1 = findViewById(R.id.question1);
+        final RadioButton rb2 = findViewById(R.id.question2);
+        final RadioButton rb3 = findViewById(R.id.question3);
+        final RadioButton rb4 = findViewById(R.id.question4);
+
+        final CountDownTimer timer = new CountDownTimer(30000, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeText.setText("Time Left:" + l/ 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                position += 1;
+            }
+        }.start();
+
+
         Trivia question = MainActivity.triviaArrayList.get(position);
+
         if(question.getImageUrl() != null){
+
             new ImageDownloaderTask(triviaImage).execute(question.getImageUrl());
         }
 
@@ -49,10 +67,34 @@ public class Question extends AppCompatActivity {
         findViewById(R.id.NextButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("imageUrl","" + MainActivity.triviaArrayList.get(0).getImageUrl());
+
+                position += 1;
+                Trivia question = MainActivity.triviaArrayList.get(position);
+                rb1.setChecked(false);
+                rb2.setChecked(false);
+                rb3.setChecked(false);
+                rb4.setChecked(false);
+
+                if(question.getImageUrl() != null){
+                    new ImageDownloaderTask(triviaImage).execute(question.getImageUrl());
+                }
+                else{
+                    triviaImage.setImageResource(R.drawable.default_question);
+                }
+
+                textQuestion.setText(question.getText());
+                rb1.setText(question.getQuestions(0));
+                rb2.setText(question.getQuestions(1));
+                rb3.setText(question.getQuestions(2));
+                rb4.setText(question.getQuestions(3));
+
+
             }
-        });
+        }); //end next on click
 
 
-    }
-}
+    }//end on create
+
+    //make method to reset, and save answers.
+
+}//end question class
